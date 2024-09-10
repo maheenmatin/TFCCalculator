05ff358d084be261a3f8c967e20c99154e18b44f
Supermarcel10 <marcelbarlik@gmail.com>
2024-09-10 21:54:45 +0100

    - Updated API to be able to retrieve components of specific alloy.

--- src/app/api/alloys/route.ts
+++ src/app/api/alloys/route.ts
@@ -1,9 +1,25 @@
-import alloys from "@/app/data/alloys.json"
-import {Alloy} from "@/app/types";
+import {Alloy} from '@/app/types';
 import { NextResponse } from 'next/server';
+import alloys from '@/app/data/alloys.json';
 
 
-export async function GET() {
-	const alloyData: { alloys: Alloy[] } = alloys;
-	return NextResponse.json(alloyData.alloys);
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const alloyName = searchParams.get('name');
+
+	// Return the list of all alloys
+	if (!alloyName) {
+		return NextResponse.json((alloys as { alloys: Alloy[] }).alloys);
+	}
+
+	// Return details for a specific alloy
+	const alloyData = (alloys as { alloys: Alloy[] }).alloys.find(
+			(alloy) => alloy.name.toLowerCase() === decodeURIComponent(alloyName).toLowerCase()
+	);
+
+	if (!alloyData) {
+		return NextResponse.json({ error: 'Alloy not found' }, { status: 404 });
+	}
+
+	return NextResponse.json(alloyData);
 }
