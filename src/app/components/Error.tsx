2a43af31a88f9a76993b69622170ac1c71f70123
marcel <marcelbarlik@gmail.com>
2024-10-09 12:13:15 +0100

    - Made explicit spaces and apostrophe for readability and maintainability.

--- src/app/components/Error.tsx
+++ src/app/components/Error.tsx
@@ -15,9 +15,9 @@ export function ErrorComponent(props : Readonly<ErrorProps>) {
 				<div className="bg-white text-black rounded-lg mt-4">
 					<h2 className="text-xl font-bold pt-4">Struggling?</h2>
 					<p className="text-lg pt-2 p-4">
-						If this issue persists,&nbsp;
+						If this issue persists,{' '}
 						<a href="https://github.com/Supermarcel10/TFGCalculator/issues/new/choose" target="_blank" className="text-cyan-500"> open an issue </a>
-						&nbsp;with a screenshot of the entire page; we&apos;ll investigate!
+						{' '}with a screenshot of the entire page; we{'\''}ll investigate!
 					</p>
 				</div>
 			</div>
