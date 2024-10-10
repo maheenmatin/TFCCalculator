6dde0ad9a1abac09c2cd3cf5536115f7943f3315
Supermarcel10 <marcelbarlik@gmail.com>
2024-10-10 11:18:18 +0100

    - Split out output into own component

--- src/app/components/AlloyComponentDisplay.tsx
+++ src/app/components/AlloyComponentDisplay.tsx
@@ -1,5 +1,6 @@
 import {ErrorComponent} from "@/app/components/ErrorComponent";
 import {MineralAccordion} from "@/app/components/MineralAccordion";
+import {OutputResultComponent} from "@/app/components/OutputResultComponent";
 import {AlloyProductionResult, calculateAlloy, MineralWithQuantity} from "@/app/functions/algorithm";
 import {Alloy, Mineral} from "@/app/types";
 import React, {useEffect, useState} from "react";
@@ -119,34 +120,7 @@ export function AlloyComponentDisplay({alloy} : Readonly<AlloyDisplayProps>) {
 					</div>
 
 					<ErrorComponent error={error}/>
-
-					{result && !error && <div className={`rounded-lg shadow p-6 ${
-							result.success
-							? "bg-green-700 text-white"
-							: "bg-yellow-400 text-black"
-					}`}>
-						<h2 className="text-xl text-center font-bold mb-4">OUTPUT</h2>
-						{!result.success && <p className="text-lg text-center">{result.message}!</p>}
-						{result.success && <div>
-							<p className="text-xl text-center">Yields exactly {result.outputMb / mbPerIngot} ingots!</p>
-							<div className="p-4">
-								<div className="flex flex-wrap justify-center gap-4">
-									{result.usedMinerals.map(usedMineral => {
-										const mineralName = usedMineral.mineral.name;
-										const mineralQuantity = usedMineral.quantity;
-
-										return (
-												<div key={mineralName}
-												     className="bg-white text-black rounded-lg flex flex-col text-center w-full md:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1rem)]">
-													<p className="mt-3 text-lg">{mineralName}</p>
-													<p className="mb-3 text-sm">x{mineralQuantity}</p>
-												</div>
-										)
-									})}
-								</div>
-							</div>
-						</div>}
-					</div>}
+					{!error && <OutputResultComponent output={result} mbPerIngot={mbPerIngot}/>}
 
 					{alloyMixture && alloyMinerals && <div className="bg-white text-black rounded-lg shadow p-6">
 						<h2 className="text-xl text-center font-bold mb-4">INPUT</h2>
