e2812d7df2f41915448ed111d0f96c77aa8282a8
Supermarcel10 <marcelbarlik@gmail.com>
2024-11-03 23:51:29 +0000

    - Created OG Meta tags. - Created API for retrieving basic generated cover image for OG Meta tag.

---
+++ src/app/api/og/route.tsx
@@ -0,0 +1,71 @@
+import {ImageResponse} from "next/og";
+
+
+export const runtime = "edge";
+
+export async function GET() {
+	return new ImageResponse(
+			(
+					<div
+							style={{
+								display : "flex",
+								background : "#f6f6f6",
+								width : "1200px",
+								height : "630px",
+								alignItems : "center",
+								justifyContent : "center",
+								padding : "48px"
+							}}
+					>
+						<div
+								style={{
+									display : "flex",
+									flexDirection : "column",
+									alignItems : "center",
+									gap : "24px"
+								}}
+						>
+							<div
+									style={{
+										display : "flex",
+										imageRendering : "pixelated",
+										width : "256px",
+										height : "256px"
+									}}
+							>
+								{/* eslint-disable-next-line @next/next/no-img-element */}
+								<img
+										src="/favicon.ico"
+										alt="TFC Alloy Calculator Logo"
+										width={"256"}
+										height={"256"}
+										style={{
+											imageRendering : "pixelated"
+										}}
+								/>
+							</div>
+							<h1
+									style={{
+										fontSize : "64px",
+										color : "#333"
+									}}
+							>
+								TerraFirmaCraft Alloy Calculator
+							</h1>
+							<p
+									style={{
+										fontSize : "32px",
+										color : "#666"
+									}}
+							>
+								A simple website to calculate minerals required for alloy compositions!
+							</p>
+						</div>
+					</div>
+			),
+			{
+				width : 1200,
+				height : 630
+			}
+	);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,30 @@ import React from "react";
 
 
 export const metadata : Metadata = {
-	title : "TerraFirmaGreg Alloy Calculator",
-	description : "Calculate alloy recipes for TerraFirmaGreg"
+	title : "TerraFirmaCraft Alloy Calculator",
+	description : "A simple website to calculate minerals required for alloy compositions!",
+	openGraph : {
+		title : "TerraFirmaCraft Alloy Calculator",
+		description : "A simple website to calculate minerals required for alloy compositions!",
+		url : "https:///tfg-calculator.vercel.app/",
+		siteName : "TerraFirmaCraft Alloy Calculator",
+		images : [
+			{
+				url : "/api/og",
+				width : 1200,
+				height : 630,
+				alt : "TFC Alloy Calculator Cover"
+			}
+		],
+		locale : "en_US",
+		type : "website"
+	},
+	twitter : {
+		card : "summary_large_image",
+		title : "TerraFirmaCraft Alloy Calculator",
+		description : "A simple website to calculate minerals required for alloy compositions!",
+		images: ['/api/og'],
+	}
 };
 
 export default function RootLayout(
