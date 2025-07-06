a26fb82987448883d79b559c308fdbd66e05fd83
Supermarcel10 <marcelbarlik@gmail.com>
2025-07-06 19:09:10 +0100

    - Added subheading property to HeadingWithBackButton component

--- src/components/HeadingWithBackButton.tsx
+++ src/components/HeadingWithBackButton.tsx
@@ -5,6 +5,7 @@ import {useRouter} from "next/navigation";
 
 interface HeadingWithButtonProps {
 	title : string;
+ 	subheading? : string;
 	ariaPreviousScreenName? : string;
 	handleBackURI : string;
 	className? : string;
@@ -13,6 +14,7 @@ interface HeadingWithButtonProps {
 export function HeadingWithBackButton(
 		{
 			title,
+			subheading,
 			ariaPreviousScreenName,
 			handleBackURI,
 			className
@@ -27,25 +29,31 @@ export function HeadingWithBackButton(
 	};
 
 	return (
-			<div className={`${className} flex items-center mb-8`}>
-				<button
-						onClick={handleBack}
-						className="mr-4"
-						aria-label={ariaLabel}
-				>
-					<CaretCircleLeftIcon
-							size={40}
-							weight="bold"
-							className="text-primary text-teal-100 hover:text-teal-300 transition-colors duration-200"
-							aria-hidden="true"
-					/>
-				</button>
+			<div className="mb-8">
+				<div className={`${className} flex items-center`}>
+					<button
+							onClick={handleBack}
+							className="mr-4"
+							aria-label={ariaLabel}
+					>
+						<CaretCircleLeftIcon
+								size={40}
+								weight="bold"
+								className="text-primary text-teal-100 hover:text-teal-300 transition-colors duration-200"
+								aria-hidden="true"
+						/>
+					</button>
 
-				<h1 className="text-3xl font-bold text-primary flex-grow text-center">
-					{title}
-				</h1>
+					<h1 className="text-3xl font-bold text-primary flex-grow text-center">
+						{title}
+					</h1>
 
-				<div className="w-10" aria-hidden="true"/>
+					<div className="w-10" aria-hidden="true"/>
+				</div>
+
+				{subheading && <h2 className="text-sm text-gray-400 text-center">
+					{subheading}
+				</h2>}
 			</div>
 	);
 }
