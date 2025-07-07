e1bee2854e68d187ea8fd71d2af1a42cbf84f033
Supermarcel10 <marcelbarlik@gmail.com>
2025-07-07 01:06:02 +0100

    - Fixed redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-    devIndicators: false
-};
+    devIndicators : false,
+    async redirects() {
+        return [{
+            source : "/modpack/terrafirmagreg/1.20.x_0.7.14/alloys/:path*",
+            destination : "/modpack/terrafirmagreg/1.20.x_0.7.x/metals/:path*",
+            permanent : true
+        }, {
+            source : "/modpack/terrafirmagreg/1.20.x_0.7.14/metals/:path*",
+            destination : "/modpack/terrafirmagreg/1.20.x_0.7.x/metals/:path*",
+            permanent : true
+        }]
+    }
+}
 
-export default nextConfig;
+export default nextConfig
