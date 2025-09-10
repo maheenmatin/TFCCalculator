3fb3909fdf53ae2112530a43e663f5ea6321d353
marcel <47798787+Supermarcel10@users.noreply.github.com>
2025-09-10 22:45:32 +0100

    - Cleanup redundant css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,21 +23,21 @@ export default function Home() {
 			role="main"
 			aria-label="Metal Calculator Home"
 		>
-			<div className="min-h-screen flex flex-col">
-				<div className="flex-1 mt-20 flex items-center justify-center">
-					<div className="max-w-6xl text-center mx-auto mb-4">
-						<h1 className="justify-center px-8 mx-auto text-5xl font-bold text-teal-300 mb-8">
+			<div className="min-h-screen flex flex-col items-center">
+				<div className="flex-1 flex items-center mt-16">
+					<div className="text-center mx-auto">
+						<h1 className="px-8 text-5xl font-bold text-teal-300 mb-8">
 							TerraFirmaCraft Calculator
 						</h1>
 						<GameVersionSelector />
 					</div>
 				</div>
-				<div className="flex justify-center mb-10">
+				<div className="flex mb-10">
 					<svg className="transition-transform duration-300 ease-out [.scroll-down_&]:scale-y-[-1]" enable-background="new 0 0 48 48" height="48px" id="Layer_1" version="1.1" viewBox="0 0 48 48" width="24px" ><g id="Layer_4"><polygon fill="#46ecd5" points="47.993,14.121 45.872,12 45.863,12.009 43.752,9.896 24.008,29.641 4.248,9.881 0.007,14.125    0.013,14.13 0.009,14.134 21.679,35.803 21.664,35.816 23.967,38.119 23.98,38.105 23.994,38.119 25.021,37.093 25.029,37.104    47.993,14.141 47.982,14.131  "/></g></svg>
 				</div>
 			</div>
 
-			<div className="max-w-6xl text-center px-6 mx-auto mt-4 mb-12">
+			<div className="text-center mx-auto mt-4 mb-12">
 				<h3 className="text-2xl font-bold mb-5 text-teal-300">Introducing TFC Calculator!</h3>
 				<div className="flex justify-center">
 					<p className="text-xl max-w-prose">
@@ -48,7 +48,7 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className="max-w-6xl text-center px-6 mx-auto mb-12">
+			<div className="text-center mx-auto mb-12">
 				<h3 className="text-2xl font-bold mb-5 text-teal-300">Any Suggestions?</h3>
 				<div className="flex justify-center">
 					<p className="text-xl max-w-prose">
